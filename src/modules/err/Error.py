bd6e93cad3aa79e509641d5bcd7ff9c349b3b7e9
Arnau478 <arnauxabia@gmail.com>
2022-02-27 19:56:21 +0100

    Colorized error output

--- src/modules/err/Error.py
+++ src/modules/err/Error.py
@@ -5,12 +5,12 @@ class Error:
         self.line = line
     
     def __repr__(self):
-        error = '\x1b[31mERROR\x1b[0m '
+        error = '\x1b[31m\x1b[1mERROR\x1b[0m '
 
         if(self.line):
-            error += f'[line {self.line}] '
+            error += f'\x1b[33m[line {self.line}]\x1b[0m '
         
-        error += f'{self.type}'
+        error += f'\x1b[1m{self.type}\x1b[0m'
 
         if(self.message):
             error += f': {self.message}'
