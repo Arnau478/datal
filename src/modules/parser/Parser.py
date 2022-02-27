b51000bc383c218567843d024a5c406096f15425
Arnau478 <arnauxabia@gmail.com>
2022-02-27 20:04:56 +0100

    New parser

--- src/modules/parser/Parser.py
+++ src/modules/parser/Parser.py
@@ -11,68 +11,114 @@ class Parser:
         self.idx = -1
         self.line = 0
         self.advance()
-    
+
     def advance(self):
-        if(self.idx < len(self.tokens)-1):
+        if(self.idx < len(self.tokens)):
             self.idx += 1
             self.current_tok = self.tokens[self.idx]
-            if(self.idx < len(self.tokens)-2):
-                self.next_token = self.tokens[self.idx + 1]
+            if(self.idx < len(self.tokens)-1):
+                self.next_tok = self.tokens[self.idx+1]
+            else:
+                self.next_tok = None
             self.line = self.current_tok.line
         else:
             self.current_tok = None
             self.next_tok = None
+        
+    def advance_n(self, num):
+        for i in range(num):
+            self.advance()
 
     def parse(self):
-        return self.expression()
+        while(self.current_tok.type != TokenType.EOF and self.current_tok != None):
+            if(self.current_tok.type == TokenType.IDENTIFIER and self.next_tok.type == TokenType.EQ):
+                definition = self.par_definition()
+                if(definition.error): return definition
+                return ParseResult(definition.result)
+            else:
+                return ParseResult(error=ParseError("Expected definition", self.line))
 
-    def expression(self):
-        return self.equality()
+    def par_expression(self):
+        lam = self.par_lambda()
+        if(lam.error): return lam
+        return ParseResult(lam.result)
     
-    def equality(self):
-        return self.bin_op(self.comparison, [TokenType.EQ_EQ, TokenType.NOT_EQ])
-    
-    def comparison(self):
-        return self.bin_op(self.term, [TokenType.GREATER, TokenType.GREATER_EQ, TokenType.LESS, TokenType.LESS_EQ])
-    
-    def term(self):
-        return self.bin_op(self.factor, [TokenType.PLUS, TokenType.MINUS])
-    
-    def factor(self):
-        return self.bin_op(self.unary, [TokenType.MUL, TokenType.DIV])
-    
-    def unary(self):
-        if(self.current_tok.type in [TokenType.MINUS]):
-            op_tok = self.current_tok
-            self.advance()
-            right = self.unary()
-            if(right.error): return right
-            right_node = right.result
-            return ParseResult(result=Expr.Unary(op_tok, right_node))
-        else:
-            return self.atom()
+    def par_lambda(self):
+        args = []
 
-    def atom(self):
-        if(self.current_tok.type in [TokenType.NUMBER]):
-            literal = Expr.Literal(self.current_tok)
-            self.advance()
-            return ParseResult(result=literal)
-        return ParseResult(error=ParseError("Expected number literal", self.line))
-    
-    def bin_op(self, left, op_toks, right=None):
-        if(right == None):
-            right = left
-
-        left_node = left();
-        if(left_node.error): return left_node
+        if(self.current_tok.type != TokenType.LPAREN):
+            return ParseResult(error=ParseError("Expected '('", self.line))
         
-        while(self.current_tok.type in op_toks):
-            op_tok = self.current_tok
+        self.advance() # Advance '('
+
+        while(self.current_tok.type != TokenType.RPAREN and self.current_tok.type != TokenType.EOF):
+            if(self.current_tok.type == TokenType.EOF): # Unclosed parenthesis
+                return ParseResult(error=ParseError("Expected ')'", self.line))
+            
+            if(self.current_tok.type == TokenType.COMMA):
+                self.advance()
+
+            # TODO: Test if this works with no arguments
+            if(self.current_tok.type != TokenType.IDENTIFIER): # If not an identifier
+                return ParseResult(error=ParseError("Expected identifier", self.line))
+            
+            argname = self.current_tok # Save argument name token
+            self.advance() # Consume identifier
+
+            if(self.current_tok.type != TokenType.COLON): # Unclosed parenthesis
+                return ParseResult(error=ParseError("Expected ':'", self.line))
             self.advance()
 
-            right_node = right()
-            if(right_node.error): return right_node
+            argtype = self.par_type()
+            if(argtype.error): return argtype
 
-            left_node = ParseResult(result=Expr.Binary(left_node.result, op_tok, right_node.result))
+            node = Node.Stmt.Definition(argname, argtype.result, None)
+
+            args.append(node)
         
-        return left_node
+        self.advance() # Advance ')'
+        if(self.current_tok.type != TokenType.COLON):
+            return ParseResult(error=ParseError("Expected ':'", self.line))
+        
+        self.advance()
+        
+        ret = self.par_type()
+
+        if(ret.error): return ret
+
+        return ParseResult(Node.Stmt.Function(args, ret.result))
+    
+    def par_type(self):
+        if(self.next_tok.type == TokenType.LESS): # Parametrized
+            args = []
+            id = self.current_tok
+
+            self.advance_n(2) # Advance id and '<'
+
+            while(self.current_tok.type != TokenType.GREATER):
+                if(self.current_tok.type == TokenType.EOF):
+                    return ParseResult(error=ParseError("Expected '>'", self.line))
+                if(self.current_tok.type == TokenType.COMMA):
+                    self.advance() # Advance ','
+                
+                type = self.par_type()
+                if(type.error): return type
+                args.append(type.result)
+                
+            node = Node.Type.Parameterized(id, args)
+            self.advance() # Advance '>'
+            return ParseResult(node)
+        node = Node.Type.Simple(self.current_tok)
+        self.advance()
+        return ParseResult(node)
+
+    def par_definition(self):
+        id = self.current_tok
+        self.advance()
+        if(self.current_tok.type != TokenType.EQ):
+            return ParseResult(error=ParseError("Expected '='", self.line))
+        self.advance()
+        
+        expr = self.par_expression()
+        if(expr.error): return expr
+        return ParseResult(Node.Stmt.Definition(id, None, expr.result)) # TODO: FIX THIS
--- src/modules/struct/SyntaxNodes.py
+++ src/modules/struct/SyntaxNodes.py
@@ -1,24 +1,34 @@
-class Expr:
-    class Binary:
-        def __init__(self, left, op_tok, right):
-            self.left = left
-            self.op_tok = op_tok
-            self.right = right
-        
-        def __repr__(self):
-            return f"({self.left}, {self.op_tok}, {self.right})"
+class Node:
+    class Stmt:
+        class Definition:
+            def __init__(self, target_tok, type, value):
+                self.target_tok = target_tok
+                self.type = type
+                self.value = value
+            
+            def __repr__(self):
+                return f"DEF({self.target_tok}, {self.type}, {self.value})"
 
-    class Unary:
-        def __init__(self, op_tok, right):
-            self.op_tok = op_tok
-            self.right = right
-        
-        def __repr__(self):
-            return f"({self.op_tok}, {self.right})"
+        class Function:
+            def __init__(self, argdefs, type):
+                self.argdefs = argdefs
+                self.type = type
+            
+            def __repr__(self):
+                return f"FUN({self.argdefs}, {self.type})"
 
-    class Literal:
-        def __init__(self, tok):
-            self.tok = tok
-        
-        def __repr__(self):
-            return f"{self.tok}"
+    class Type:
+        class Simple:
+            def __init__(self, id):
+                self.id = id
+
+            def __repr__(self):
+                return f"S_TYPE({self.id})"
+
+        class Parameterized:
+            def __init__(self, id, args):
+                self.id = id
+                self.args = args
+
+            def __repr__(self):
+                return f"P_TYPE({self.id}, {self.args})"
