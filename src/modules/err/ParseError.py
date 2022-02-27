a63f32ccf2252ef5dc35073da1f98fa7f5fed83e
Arnau478 <arnauxabia@gmail.com>
2022-02-27 19:57:04 +0100

    Fixed error name

--- src/modules/err/ParseError.py
+++ src/modules/err/ParseError.py
@@ -2,6 +2,6 @@ from modules.err.Error import *
 
 class ParseError(Error):
     def __init__(self, message, line):
-        super().__init__(__name__, message, line)
+        super().__init__(self.__class__.__name__, message, line)
 
     def __repr__(self): return super().__repr__()
